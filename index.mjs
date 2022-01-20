a77ded890e377b11efb5c112bc0b7af214c52d44
dedydantry <dedy@digitalstrategy.id>
2022-01-21 00:03:43 +0700

    add cors

--- index.mjs
+++ index.mjs
@@ -1,11 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import ApiRouter from './router/api.mjs'
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
 const PORT = process.env.APP_PORT
 const HOST = process.env.APP_HOST
+app.use(cors())
 app.use(express.json())
 
 app.use('/api', ApiRouter)
