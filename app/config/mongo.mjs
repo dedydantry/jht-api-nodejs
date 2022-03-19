d528b5be0444c9fa89f1acc0356698c758346b1d
dedydantry <dedydantry@gmail.com>
2022-03-19 21:38:24 +0700

    fix config mongo

--- app/config/mongo.mjs
+++ app/config/mongo.mjs
@@ -2,9 +2,10 @@ import mongoose from "mongoose"
 import dotenv from "dotenv"
 dotenv.config()
 
+console.log(`mongodb://${process.env.MONGO_NAME}:${encodeURIComponent(process.env.MONGO_NAME)}@8.215.28.69:27017/${process.env.MONGO_DB}`, 'oke');
 try {
    mongoose
-    .connect(`mongodb://${process.env.MONGO_NAME}:${encodeURIComponent(process.env.MONGO_NAME)}@8.215.28.69:27017/${process.env.MONGO_DB}`, {
+    .connect(`mongodb://${process.env.MONGO_NAME}:${encodeURIComponent(process.env.MONGO_PASSWORD)}@8.215.28.69:27017/${process.env.MONGO_DB}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useNewUrlParser:true
