97942a09d89f0eccf93f3fbadef89b6f1fb2f957
dedydantry <dedydantry@gmail.com>
2022-04-15 13:21:02 +0700

    particiapnt store

--- app/controller/ParticipantController.mjs
+++ app/controller/ParticipantController.mjs
@@ -11,49 +11,40 @@ const ParticipantController = {
                 name:'required',
                 email:'required',
                 phone:'required',
-                event_id:'required'
+                participants:'required',
+                event:'required'
             })
             const matched = await validate.check();
             if(!matched) return res.send({
                 status:false,
                 message:errorValidations(validate.errors)
             })
-
-            const event = await Event.findById(req.body.event_id)
+            const event = await Event.findOne({slug:req.body.event})
             if(!event) return res.send({
                 status:false,
-                message:'Invalid Event'
+                message:'Invalid event'
             })
 
-            const checkParticipant = await Event.findOne({_id:req.body.event_id, "participants": {$elemMatch:{email:req.body.email}}})
-            let order = null
-            if(!checkParticipant){
-                const params = {
-                    name:req.body.name,
-                    email:req.body.email,
-                    phone:req.body.phone,
-                    created_at:format(new Date(), 'yyyy-mm-dd H:m:s')
-                }
-                await Event.findByIdAndUpdate(req.body.event_id, 
-                    { "$push": { "participants": params}},
-                    { "new": true, "upsert": true }
-                )
-                
-                const paramsOrder = {
-                    invoice:Date.now(),
-                    email:req.body.email,
-                    total:event.price,
-                    event_id:req.body.event_id
-                }
-                order = new EventOrder(paramsOrder)
-                order.save()
-            }else{
-                order = await EventOrder.findOne({
-                    event_id:req.body.event_id,
-                    email:req.body.email
-                })
+            const params = {
+                name:req.body.name,
+                email:req.body.email,
+                phone:req.body.phone,
+                participants:req.body.participants,
+                created_at:format(new Date(), 'yyyy-mm-dd H:m:s')
             }
-
+            await Event.findByIdAndUpdate(event._id, 
+                { "$push": { "participants": params}},
+                { "new": true, "upsert": true }
+            )
+                
+            const paramsOrder = {
+                invoice:Date.now(),
+                email:req.body.email,
+                total:event.price,
+                event_id:event._id
+            }
+            const order = new EventOrder(paramsOrder)
+            order.save()
             return res.send({
                 status:true,
                 message:{
