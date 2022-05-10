b76edea4ce000944a74b234ec7d31de060e9491b
Jeffsitorus <jefrisitorus22@gmail.com>
2022-05-10 21:42:38 +0700

    fix

--- app/controller/EventOrderController.mjs
+++ app/controller/EventOrderController.mjs
@@ -2,12 +2,29 @@ import Event from '../models/mongo/Events.mjs'
 import { Validator } from 'node-input-validator';
 import { errorValidations } from '../helpers/index.mjs';
 const EventOrderController = {
+  async show(req, res) {
+    try {
+      const invoice = req.params.invoice;
+      console.log('invoice', invoice);
+      const event = await Event.findOne({ "participants.invoice":invoice },{'participants.$':1});
+      return res.send({
+        status: true,
+        message: event,
+      });
+    } catch (error) {
+      return res.send({
+        status: false,
+        message: error.message,
+      });
+    }
+  },
+
   async update(req, res) {
     try {
       const validate = new Validator(req.body, {
-        paid_at: 'required',
-        paid_by: 'required',
-        payment_payload: 'required',
+        paid_at: "required",
+        paid_by: "required",
+        payment_payload: "required",
       });
       const matched = await validate.check();
       if (!matched) {
@@ -19,11 +36,16 @@ const EventOrderController = {
 
       const invoice = req.params.invoice;
 
-      await Event.updateOne({'participants.invoice': invoice}, {'$set':{
-        'participants.$.paid_at' : req.body.paid_at,
-        'participants.$.paid_by' : req.body.paid_by,
-        'participants.$.payment_payload' : req.body.payment_payload,
-      }})
+      await Event.updateOne(
+        { "participants.invoice": invoice },
+        {
+          $set: {
+            "participants.$.paid_at": req.body.paid_at,
+            "participants.$.paid_by": req.body.paid_by,
+            "participants.$.payment_payload": req.body.payment_payload,
+          },
+        }
+      );
 
       return res.send({
         status: true,
